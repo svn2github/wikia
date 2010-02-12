svn2github/wikia

commit 458dd279ac74f1f788187ac185eca828e5544139

#39265: prevent fatals on nonexisting useskins
git-svn-id: https://svn.wikia-code.com/wikia/trunk@18371 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/SkinChooser/SkinChooser.php
@@ -308,13 +308,14 @@ function WikiaGetSkin ($user) {
 			return false;
 		}
 	}
-
 	if(!empty($wgForceSkin)) {
-		$elems = preg_split('/-/', $wgForceSkin);
+		$wgForceSkin = $wgRequest->getVal('useskin', $wgForceSkin);
+        $elems = preg_split('/-/', $wgForceSkin);
 		$userSkin = ( array_key_exists(0, $elems) ) ? $elems[0] : null;
 		$userTheme = ( array_key_exists(1, $elems) ) ? $elems[1] : null;
-		$user->mSkin = &Skin::newFromKey($wgRequest->getVal('useskin', $userSkin));
-		$user->mSkin->themename = $wgRequest->getVal('usetheme', $userTheme);
+        
+		$user->mSkin = &Skin::newFromKey($userSkin);
+		$user->mSkin->themename = $userTheme;
 		wfProfileOut(__METHOD__);
 		return false;
 	}
@@ -381,7 +382,10 @@ function WikiaGetSkin ($user) {
 	}
 	wfProfileOut(__METHOD__.'::GetSkinLogic');
 
-	$userSkin = $wgRequest->getVal('useskin', $userSkin);
+    $useskin = $wgRequest->getVal('useskin', $userSkin);
+    $elems = preg_split('/-/', $wgForceSkin);
+    $userSkin = ( array_key_exists(0, $elems) ) ? $elems[0] : null;
+    $userTheme = ( array_key_exists(1, $elems) ) ? $elems[1] : null;                                                         
 	$userTheme = $wgRequest->getVal('usetheme', $userTheme);
 
 	if(empty($userTheme) && strpos($userSkin, 'quartz-') === 0) {

commit 20cad669b2cf8282fc32f9136a5c55c970e4ff76

Something broke in r18371 and this fixes it and I _think_ it still keeps the fix which 18371 was trying to do.  Will get a code-review.
git-svn-id: https://svn.wikia-code.com/wikia/trunk@18397 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/SkinChooser/SkinChooser.php
@@ -310,7 +310,7 @@ function WikiaGetSkin ($user) {
 	}
 	if(!empty($wgForceSkin)) {
 		$wgForceSkin = $wgRequest->getVal('useskin', $wgForceSkin);
-        $elems = preg_split('/-/', $wgForceSkin);
+		$elems = preg_split('/-/', $wgForceSkin);
 		$userSkin = ( array_key_exists(0, $elems) ) ? $elems[0] : null;
 		$userTheme = ( array_key_exists(1, $elems) ) ? $elems[1] : null;
         
@@ -383,9 +383,9 @@ function WikiaGetSkin ($user) {
 	wfProfileOut(__METHOD__.'::GetSkinLogic');
 
     $useskin = $wgRequest->getVal('useskin', $userSkin);
-    $elems = preg_split('/-/', $wgForceSkin);
+    $elems = preg_split('/-/', $useskin);
     $userSkin = ( array_key_exists(0, $elems) ) ? $elems[0] : null;
-    $userTheme = ( array_key_exists(1, $elems) ) ? $elems[1] : null;                                                         
+    $userTheme = ( array_key_exists(1, $elems) ) ? $elems[1] : $userTheme;                                                         
 	$userTheme = $wgRequest->getVal('usetheme', $userTheme);
 
 	if(empty($userTheme) && strpos($userSkin, 'quartz-') === 0) {