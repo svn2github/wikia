svn2github/wikia

commit b6be8d793cc4f5eeab630c53819b575a643cc84d

Partial revert of r27338.
I assume it accidentally reverted r27337 and part of r27042 as it was not mentioned in the commit message and not relevant to adding the message key for the special page name/title.

git-svn-id: https://svn.wikia-code.com/wikia/trunk@27354 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/PartnerFeed/PartnerFeed.i18n.php
@@ -13,15 +13,15 @@ $messages = array();
  * @return array
  */
 $messages['en'] = array(
-	'error-no-article'		=> 'Error: No "article" param passed',
-	'feed-title-blogposts'		=> 'Recent blog posts',
-	'feed-title-recent-badges'	=> 'Recent badges',
-	'feed-title-recent-images'	=> 'Recent images',
-	'feed-title-leaderboard'	=> 'Achivements leaderboard',
-	'feed-main-title'		=> 'Wikia partner feed',
-	'all-blog-posts'		=> 'all categories',
-	'blog-posts-from-listening'	=> 'from listening: $1',
-	'feed-title-hot-content'	=> 'Hot content - $1',
-	'feed-title-blogcomments'	=> 'Comments to $1',
-	'partnerfeed'			=> 'Partner Feed'
+	'partnerfeed'               => 'Partner feed'
+	'error-no-article'          => 'Error: No "article" param passed',
+	'feed-title-blogposts'      => 'Recent blog posts',
+	'feed-title-recent-badges'  => 'Recent badges',
+	'feed-title-recent-images'  => 'Recent images',
+	'feed-title-leaderboard'    => 'Achievements leaderboard',
+	'feed-main-title'           => 'Wikia partner feed',
+	'all-blog-posts'            => 'all categories',
+	'blog-posts-from-listening' => 'from listening: $1',
+	'feed-title-hot-content'    => 'Hot content - $1',
+	'feed-title-blogcomments'   => 'Comments to $1',
 );

commit 12abb1a74cba9484dde8ce97b8ea7d827688016a

Fix syntax error introduced in r27354.
git-svn-id: https://svn.wikia-code.com/wikia/trunk@27355 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/PartnerFeed/PartnerFeed.i18n.php
@@ -13,7 +13,7 @@ $messages = array();
  * @return array
  */
 $messages['en'] = array(
-	'partnerfeed'               => 'Partner feed'
+	'partnerfeed'               => 'Partner feed',
 	'error-no-article'          => 'Error: No "article" param passed',
 	'feed-title-blogposts'      => 'Recent blog posts',
 	'feed-title-recent-badges'  => 'Recent badges',