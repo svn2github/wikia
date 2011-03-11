svn2github/wikia

commit 179b47b6300669aea57ee662cfc5b1ae7dfd3d4d

BugId: 3319
git-svn-id: https://svn.wikia-code.com/wikia/trunk@34459 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/EditPage.php
@@ -1149,7 +1149,18 @@ class EditPage {
 	 */
 	function initialiseForm() {
 		global $wgUser;
-		$this->edittime = $this->mArticle->getTimestamp();
+		
+		
+		//Wikia Change
+		//https://wikia.fogbugz.com/default.asp?3319#21795
+		if ( $this->mArticle->mRevision->isCurrent() ) {
+			$this->edittime = $this->mArticle->getTimestamp();	
+		} else {
+			$rev = Revision::newFromTitle( $this->mTitle );
+			$this->edittime = $rev->getTimestamp();
+		}
+		//
+		
 		$this->textbox1 = $this->getContent( false );
 		// activate checkboxes if user wants them to be always active
 		# Sort out the "watch" checkbox

commit 32a2aa3eb62a4103c417c90add95610b8dad899f

EditPage: fixing regression introduced with r34459 (BugId:3319)
git-svn-id: https://svn.wikia-code.com/wikia/trunk@34525 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/EditPage.php
@@ -1153,7 +1153,7 @@ class EditPage {
 		
 		//Wikia Change
 		//https://wikia.fogbugz.com/default.asp?3319#21795
-		if ( $this->mArticle->mRevision->isCurrent() ) {
+		if ( ( !empty( $this->mArticle->mRevision ) && $this->mArticle->mRevision->isCurrent() ) ||  empty( $this->mArticle->mRevision )  ) {
 			$this->edittime = $this->mArticle->getTimestamp();	
 		} else {
 			$rev = Revision::newFromTitle( $this->mTitle );