svn2github/wikia

commit 7a3b7f0fd1b03f0310ba3111a5bdfdc555297e0a

trunk: moved Module compat code to Module class
git-svn-id: https://svn.wikia-code.com/wikia/trunk@36725 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/wikia/Module.php
@@ -21,6 +21,46 @@ abstract class Module extends WikiaController {
 		}
 	}
 
+	/* This magic function allows the Module to pretend to be a Response object
+	 * We will pass all calls except getData() render() toString() to the real Response object
+	 * Dispatcher is none the wiser, and treats the Module as a response object
+	 */
+	protected $realResponse;
+
+	public function __call($method, $args) 
+	{ 
+		if (method_exists($this->realResponse,$method)) 
+			return $this->realResponse->$method($args); 
+		else 
+			throw new WikiaException( sprintf('Response Proxy failed for Method: %s', $method) );
+	}		
+			
+	// Save the real response object so we can act as a proxy
+	public function setResponse(WikiaResponse &$response) {
+		$this->realResponse = $response;
+		$this->response = $this;
+		$response = $this;
+	}
+
+	public function printText() {
+		$this->realResponse->setData($this->getData());
+		print $this->realResponse->toString();				
+	}
+	
+	public function render() {
+		$this->realResponse->setData($this->getData());
+		print $this->realResponse->toString();		
+	}
+	
+	public function toString() {
+		$this->realResponse->setData($this->getData());
+		return $this->realResponse->toString();
+	}
+	
+	public function getVal($key, $default = null) {
+		return isset($this->$key) ? $this->$key : $default;
+	}
+	
 	public static function setSkinTemplateObj(&$skinTemplate) {
 		self::$skinTemplateObj = $skinTemplate;
 	}
@@ -29,48 +69,9 @@ abstract class Module extends WikiaController {
 		return self::$skinTemplateObj;
 	}
 
-	// TODO: This function goes away, replaced by Dispatcher::dispatch
+	// TODO: This function goes away when all usages are replaced by Controller::sendRequest
 	public static function get($name, $action = 'Index', $params = null) {
-		global $wgAutoloadClasses;
-
-		$moduleClassName = $name.'Module';
-
-		if( !class_exists( $moduleClassName ) ) {
-			return null;
-		}
-		wfProfileIn(__METHOD__ . " (" . $name.'_'.$action .")");
-
-		//$moduleObject = new $moduleClassName();
-		$moduleObject = F::build($moduleClassName);
-		$moduleObject->templatePath = dirname($wgAutoloadClasses[$moduleClassName]).'/templates/'.$name.'_'.$action.'.php';
-
-		// auto-initialize any module variables which match variables in skinTemplate->data or _GLOBALS
-		$objvars = get_object_vars($moduleObject);
-		$skindata = array();
-		if (isset(self::$skinTemplateObj) && is_array(self::$skinTemplateObj->data)) {
-			$skindata = self::$skinTemplateObj->data;
-		}
-		foreach ($objvars as $var => $unused) {
-			if (array_key_exists($var, $GLOBALS)) {
-				$moduleObject->$var = $GLOBALS[$var];
-			}
-			if (array_key_exists($var, $skindata)) {
-				$moduleObject->$var = $skindata[$var];
-			}
-		}
-
-		if(wfRunHooks($name.$action.'BeforeExecute', array(&$moduleObject, &$params))) {
-			$actionName = 'execute'.$action;
-
-			// BugId:2649
-			if (method_exists($moduleObject, $actionName)) {
-				$moduleObject->$actionName($params);
-			}
-		}
-		wfRunHooks($name.$action.'AfterExecute', array(&$moduleObject, &$params));
-
-		wfProfileOut(__METHOD__ . " (" . $name.'_'.$action .")");
-		return $moduleObject;
+		return F::app()->sendRequest($name, $action, $params);		
 	}
 
 	public function getData($var = null) {
@@ -81,13 +82,4 @@ abstract class Module extends WikiaController {
 			return isset($vars[$var]) ? $vars[$var] : null;
 		}
 	}
-	/*
-	public function getView() {
-		return new View($this->templatePath, $this->getData());
-	}
-
-	public function render() {
-		return $this->getView()->render();
-	}
-	*/
 }

includes/wikia/WikiaDispatcher.class.php
@@ -73,11 +73,6 @@ class WikiaDispatcher {
 				}
 				$app->runHook( ( $controllerName . $originalMethod . 'AfterExecute' ), array( &$controller, &$params ) );
 
-				// Work around for module dispatching until modules are renamed
-				if ($controller instanceof Module) {
-					$response->setData($controller->getData());
-				}
-
 			} catch (Exception $e) {
 				$app->runFunction( 'wfProfileOut', ( __METHOD__ . " (" . $controllerName.'_'.$method .")" ) );
 				$response->setException($e);

commit 4ced90300d898ef902edaa5de939be3d1c79250a

fixed a regression caused by r36725
git-svn-id: https://svn.wikia-code.com/wikia/trunk@36891 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/wikia/Module.php
@@ -30,7 +30,7 @@ abstract class Module extends WikiaController {
 	public function __call($method, $args) 
 	{ 
 		if (method_exists($this->realResponse,$method)) 
-			return $this->realResponse->$method($args); 
+			return call_user_func_array(array($this->realResponse,$method), $args); 
 		else 
 			throw new WikiaException( sprintf('Response Proxy failed for Method: %s', $method) );
 	}		